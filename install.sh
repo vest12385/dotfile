304bc352bedce7065a0baf1f14b28f6d60700c0d
Jerry_Chen <vest12385@gmail.com>
2018-08-11 20:22:37 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,3 +7,8 @@ sudo apt-get -y upgrade
 sudo apt-get install git -y
 sudo apt-get install vim -y
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+cd ../dotfile/
+mv ./Pwngdb/ ~/tools/
+mv ./dotfiles/ ~/tools/
+mv ./peda/ ~/tools/
+tmux
