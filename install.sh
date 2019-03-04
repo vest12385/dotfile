145d713768bb639a4e284a6026f49a5d8969b3e2
Jerry_Chen <vest12385@gmail.com>
2019-03-04 23:30:23 +0800

    Correct apt install item

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ cd ~/tools
 sudo dpkg --add-architecture i386
 sudo apt-get -y update
 sudo apt-get -y upgrade
-sudo apt-get install git vim libc6:i386 libncurses5:i386 libstdc++6:i386 livc6-dev-i386 python python-dev python-pip -y
+sudo apt-get install git vim libc6:i386 libncurses5:i386 libstdc++6:i386 libc6-dev-i386 python python-dev python-pip powerline -y
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 pip install ipython
 pip install pwntools
