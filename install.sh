b3fcc8adfe4868c7407343315aacb81398e251da
Jerry_Chen <vest12385@gmail.com>
2020-08-30 19:00:22 +0800

    modify for use

--- install.sh
+++ install.sh
@@ -1,18 +1,19 @@
 #!/bin/bash 
 #
-mkdir ~/tools
 cd ~/tools
 sudo dpkg --add-architecture i386
 sudo apt-get -y update
 sudo apt-get -y upgrade
-sudo apt-get install git vim libc6:i386 libncurses5:i386 libstdc++6:i386 libc6-dev-i386 python python-dev python-pip powerline -y
+sudo apt-get install git vim tmux libc6:i386 libncurses5:i386 libstdc++6:i386 libc6-dev-i386 python python-dev python-pip powerline -y
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 pip install ipython
-pip install pwntools
-cd ../dotfile/
+#pip install pwntools
+cd ~/tools/dotfile/
 mv ./Pwngdb/ ~/tools/
 mv ./dotfiles/ ~/tools/
 mv ./peda/ ~/tools/
+cd ~/tools/dotfiles
+make
 sudo mv ./monaco.ttf/monaco.ttf /usr/local/share/fonts/
 fc-cache -f -v
 
