c2a1a587c3c23a1c6b8d9f34db5d4d96a27bb224
jerry chen <vest12385@gmail.com>
2020-12-19 19:35:53 +0800

    modify error

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-#
+
 cd ~/tools
 sudo dpkg --add-architecture i386
 sudo apt-get -y update
@@ -8,13 +8,14 @@ sudo apt-get install git vim tmux libc6:i386 libncurses5:i386 libstdc++6:i386 li
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 pip install ipython
 #pip install pwntools
+git clone https://github.com/vest12385/dotfile ~/tools/
 cd ~/tools/dotfile/
 mv ./Pwngdb/ ~/tools/
 mv ./dotfiles/ ~/tools/
 mv ./peda/ ~/tools/
 cd ~/tools/dotfiles
 make
-sudo mv ./monaco.ttf/monaco.ttf /usr/local/share/fonts/
+sudo mv ~/tools/dotfile/monaco.ttf/monaco.ttf /usr/local/share/fonts/
 fc-cache -f -v
 
 tmux
